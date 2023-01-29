f92cdfdffe3e12107be5dd62636ba5edfa257b8d
alvaro <alvarogarciagimeno@gmail.com>
2023-01-29 12:15:25 +0100

    feat(#2):LocalStorage calorias

--- src/app/calculadoras/calculadora-completa/calculadora-completa.component.ts
+++ src/app/calculadoras/calculadora-completa/calculadora-completa.component.ts
@@ -8,14 +8,32 @@ import { Component } from '@angular/core';
 export class CalculadoraCompletaComponent {
   vPeso!:number;
   vFactorActividad!:number;
-  vObjetivo!:string;
+  vObjetivo="";
   caloriasConsumir!:number;
+  caloriasFinales!:number;
+  caloriasGuardar!:string;
+  verCalorias!:number;
   ngOnInit(){
-
+    console.log()
+    this.verCalorias=Number(localStorage.getItem("cal"))
   }
   calcular(){
     this.caloriasConsumir=0;
+    this.caloriasFinales=0;
+
+
     this.caloriasConsumir=this.vPeso*22*(this.vFactorActividad/10);
-    console.log(this.caloriasConsumir)
+    if(this.vObjetivo=="ganar"){
+      this.caloriasFinales = this.caloriasConsumir+((this.caloriasConsumir)*(15/100));
+      let guardar = this.caloriasFinales.toString();
+      localStorage.setItem("cal",guardar)
+    }
+    if(this.vObjetivo=="perder"){
+      this.caloriasFinales = this.caloriasConsumir-(this.caloriasConsumir*(20/100));
+      let guardar = this.caloriasFinales.toString();
+      localStorage.setItem("cal",guardar)
+    }
+    this.verCalorias = this.caloriasFinales;
   }
+
 }
