c87dc0a599f60434c59e77cfcbe6fb69f55cb1f1
alvaro <alvarogarciagimeno@gmail.com>
2023-01-30 20:44:47 +0100

    feat(#3):Tabla resultados

--- src/app/calculadoras/calculadora-completa/calculadora-completa.component.ts
+++ src/app/calculadoras/calculadora-completa/calculadora-completa.component.ts
@@ -3,37 +3,87 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculadora-completa',
   templateUrl: './calculadora-completa.component.html',
-  styleUrls: ['./calculadora-completa.component.css']
+  styleUrls: ['./calculadora-completa.component.css'],
 })
 export class CalculadoraCompletaComponent {
-  vPeso!:number;
-  vFactorActividad!:number;
-  vObjetivo="";
-  caloriasConsumir!:number;
-  caloriasFinales!:number;
-  caloriasGuardar!:string;
-  verCalorias!:number;
-  ngOnInit(){
-    console.log()
-    this.verCalorias=Number(localStorage.getItem("cal"))
+  vPeso!: number;
+  vFactorActividad!: number;
+  vObjetivo = '';
+  caloriasConsumir!: number;
+  caloriasFinales!: number;
+  //Variables para guardar
+  caloriasGuardadas!: number;
+  proteinasRecomendadas!: number;
+  hidratosRecomendados!: number;
+  grasasRecomendadas!: number;
+  //Cosas en calorias
+  proteinasCal!:number;
+  grasasCal!:number;
+  hidratosCal!:number;
+
+  ngOnInit() {
+    this.caloriasConsumir =Math.round( Number(localStorage.getItem('cal')));
+    this.proteinasRecomendadas =Math.round( Number(localStorage.getItem('prot')));
+    this.grasasRecomendadas =Math.round( Number(localStorage.getItem('grasas')));
+    this.hidratosRecomendados =Math.round( Number(localStorage.getItem("hidratos")));
+    this.proteinasCal = this.proteinasRecomendadas*4;
+    this.grasasCal = this.grasasRecomendadas*9;
+    this.hidratosCal = this.hidratosRecomendados*4;
   }
-  calcular(){
-    this.caloriasConsumir=0;
-    this.caloriasFinales=0;
+  calcular() {
+    let guardarCal = '';
+    let guardarProt = '';
+    let guardarGrasas = '';
+    let guardarHidratos = '';
+    if (this.vPeso==null||this.vFactorActividad==null||this.vObjetivo=="") {
+      alert('Introduce todos los campos');
+    } else {
+      this.caloriasConsumir = this.vPeso * 22 * (this.vFactorActividad / 10);
+      if (this.vObjetivo == 'ganar') {
+        //Calorias
+        this.caloriasConsumir =
+          this.caloriasConsumir + this.caloriasConsumir * (15 / 100);
+        guardarCal = this.caloriasConsumir.toString();
+        //Proteinas
+        this.proteinasRecomendadas = 2 * this.vPeso;
+        guardarProt = this.proteinasRecomendadas.toString();
+        //Grasas
+        this.grasasRecomendadas = (12/10)*this.vPeso;
+        guardarGrasas = this.grasasRecomendadas.toString();
 
-
-    this.caloriasConsumir=this.vPeso*22*(this.vFactorActividad/10);
-    if(this.vObjetivo=="ganar"){
-      this.caloriasFinales = this.caloriasConsumir+((this.caloriasConsumir)*(15/100));
-      let guardar = this.caloriasFinales.toString();
-      localStorage.setItem("cal",guardar)
+      }
+      if (this.vObjetivo == 'perder') {
+        //Calorias
+        this.caloriasConsumir =
+          this.caloriasConsumir - this.caloriasConsumir * (20 / 100);
+        guardarCal = this.caloriasConsumir.toString();
+        //Proteinas
+        this.proteinasRecomendadas = (23 / 10) * this.vPeso;
+        guardarProt = this.proteinasRecomendadas.toString();
+        //Grasas
+        this.grasasRecomendadas = (8/10)*this.vPeso;
+        guardarGrasas = this.grasasRecomendadas.toString();
+      }
+      //Calorias
+      localStorage.setItem('cal', guardarCal);
+      //Proteinas
+      localStorage.setItem('prot', guardarProt);
+      //Grasas
+      localStorage.setItem('grasas',guardarGrasas);
+      //Hidratos
+      this.hidratosCal = Math.round(this.caloriasConsumir-(this.proteinasCal+this.grasasCal));
+      this.hidratosRecomendados = Math.round(this.hidratosCal/4);
+      guardarHidratos = this.hidratosRecomendados.toString();
+      localStorage.setItem('hidratos',guardarHidratos);
+      console.log(this.caloriasConsumir-(this.proteinasCal+this.grasasCal+this.hidratosCal))
     }
-    if(this.vObjetivo=="perder"){
-      this.caloriasFinales = this.caloriasConsumir-(this.caloriasConsumir*(20/100));
-      let guardar = this.caloriasFinales.toString();
-      localStorage.setItem("cal",guardar)
-    }
-    this.verCalorias = this.caloriasFinales;
   }
+  // calculoProteinas(){
+  //   if(this.vObjetivo=="ganar"){
 
+  //   }
+  //   if(this.vObjetivo=="perder"){
+
+  //   }
+  // }
 }
